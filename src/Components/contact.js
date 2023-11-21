3ccb8f47163604fa9d1ed51170e8387806791b39
gurusai06102003 <132398700+gurusai06102003@users.noreply.github.com>
2023-11-21 22:44:33 +0530

    Update contact.js

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -21,7 +21,7 @@ function Contact(props){
                     <nav className="navbar navbar-expand-lg">
                         <div className="container-fluid">
                             <a className="navbar-brand" href="/"><b>ApnaHome</b></a>
-                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <span className="navbar-toggler-icon"></span>
                             </button>
                             <div className="collapse navbar-collapse" id="navbarSupportedContent">
